4e8ec06e211eac23575f44165e639e0409b8ea4b
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-08-19 16:03:38 -0500

    updating directory sturcture, attempting to properly implement tailwind

---
+++ client_resources/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../templates/**/*/.{html, js,ts}",
+    "/typscript/**/*/.{html,js,ts}"
+  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
